6585be7abbf68d813546c5ff82c79620e1a8f490
Hun3431 <limdohun77@gmail.com>
2025-06-16 17:37:36 +0900

    [Feat] Container 컴포넌트 추가

---
+++ src/components/component/container/index.tsx
@@ -0,0 +1,30 @@
+import { cn } from "cn-func";
+
+interface ContainerProps {
+  children?: React.ReactNode;
+  style?: React.CSSProperties;
+  id?: string;
+  className?: string;
+  onClick?: React.MouseEventHandler<HTMLDivElement>;
+}
+
+const Container = ({
+  children,
+  style,
+  id,
+  className,
+  onClick,
+}: ContainerProps) => {
+  return (
+    <div
+      id={id}
+      style={style}
+      onClick={onClick}
+      className={cn("flex-1 flex flex-col gap-3 p-6", className)}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Container;
